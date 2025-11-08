4f9c416c2e590616e5780b2b461c727e9dc67b69
namannanu <2024pietcsnaman109@poornima.org>
2025-11-08 21:36:11 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -22,6 +22,8 @@ const corsOptions = {
     'Origin',
     'X-Requested-With',
     'X-Business-Id',
+    'X-User-Id',
+    'x-user-id',
     'Access-Control-Allow-Headers',
     'Access-Control-Request-Method',
     'Access-Control-Request-Headers',
@@ -52,7 +54,10 @@ app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Business-Id');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With, X-Business-Id, X-User-Id, x-user-id'
+  );
   res.header('Access-Control-Allow-Credentials', 'true');
   
   // Handle preflight
